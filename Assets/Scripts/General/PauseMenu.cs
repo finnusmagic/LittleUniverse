0dbcb9416b552dee58bc52e77c99e4734fa45364
Finn Pinkenburg <finnpinkenburg@googlemail.com>
2017-10-05 11:32:42 +0200

    Player Setup / Skybox Imports

---
+++ Assets/Scripts/General/PauseMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isOn = false;
+
+    void Start()
+    {
+
+    }
+
+    public void TogglePauseMenu()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+        PauseMenu.isOn = gameObject.activeSelf;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour {
+
+    [SerializeField] RectTransform thrusterFuelFill;
+
+    [SerializeField] GameObject pauseMenu;
+
+    PlayerController controller;
+
+    void Update()
+    {
+        controller = FindObjectOfType<PlayerController>();
+
+        SetFuelAmount (controller.GetThrusterFuelAmount());
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+    }
+
+    void Start()
+    {
+        PauseMenu.isOn = false;
+    }
+
+    public void SetController(PlayerController _controller)
+    {
+        controller = _controller;
+    }
+
+    void SetFuelAmount(float _amount)
+    {
+        thrusterFuelFill.localScale = new Vector3(1f, _amount, 1f);
+    }
+
+    void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+        PauseMenu.isOn = pauseMenu.activeSelf;
+    }
+}
